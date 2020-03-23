9ecbee9f399c16ced3480a29235f1be757d1ffd4
Elias de Francisco <elias@iMac-de-Elias.local>
2020-03-23 18:28:53 +0100

    more data faked

--- src/fakeUser.js
+++ src/fakeUser.js
@@ -3,4 +3,15 @@ const faker = require('faker')
 const firstName = faker.name.firstName()
 const lastName = faker.name.lastName()
 
-console.log(`Employee: ${firstName} ${lastName}`)
+const jobTitle = faker.name.jobTitle()
+const jobArea = faker.name.jobArea()
+
+const prefix = faker.name.prefix()
+const suffix = faker.name.suffix()
+
+const phone = faker.name.suffix()
+
+console.log(`Employee: ${prefix} ${firstName} ${lastName} ${suffix}`)
+console.log(`Job Title: ${jobTitle}`)
+console.log(`Job Area: ${jobArea}`)
+console.log(`Phone: ${phone}`)
